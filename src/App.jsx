e93e120d330dfc520b203f2845816d90db0bed94
Yashgoswami14 <goswamiyash256@gmail.com>
2025-02-05 23:11:38 +0530

    design authSerive using appwrite and created conf file for env

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
+import conf from "./conf/conf.js"
 
 function App() {
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(conf.appwriteUrl);
   return (
     <>
       <h1>hello world!</h1>
